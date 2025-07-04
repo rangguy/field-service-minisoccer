d0fe4f604fabbc43a43aa71c45ae27d8c9f5300d
rangguy <ranggadwi100@gmail.com>
2025-07-04 11:41:53 +0700

    Setup Bagian 2

--- constants/error/error_mapping.go
+++ constants/error/error_mapping.go
@@ -1,8 +1,13 @@
 package error
 
+import (
+	error2 "field-service/constants/error/field"
+	errField "field-service/constants/error/fieldSchedule"
+)
+
 func ErrMapping(err error) bool {
 	allErrors := make([]error, 0)
-	allErrors = append(append(GeneralErrors[:], UserErrors[:]...))
+	allErrors = append(append(GeneralErrors[:], error2.FieldErrors[:]...), errField.FieldScheduleErrors[:]...)
 
 	for _, item := range allErrors {
 		if err.Error() == item.Error() {
