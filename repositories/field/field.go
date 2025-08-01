9856096b21f94771ebe1fdc30c61a165576e91c5
rangguy <ranggadwi100@gmail.com>
2025-08-01 22:32:57 +0700

    Repository Bagian 1

---
+++ repositories/field/field.go
@@ -0,0 +1,116 @@
+package repositories
+
+import (
+	"context"
+	"errors"
+	errWrap "field-service/common/error"
+	errConstant "field-service/constants/error"
+	errField "field-service/constants/error/field"
+	"field-service/domain/dto"
+	"field-service/domain/models"
+	"fmt"
+	"gorm.io/gorm"
+)
+
+type FieldRepository struct {
+	db *gorm.DB
+}
+
+type IFieldRepository interface {
+	FindAllWithPagination(context.Context, *dto.FieldRequestParam) ([]models.Field, int64, error)
+	FindAllWithOutPagination(context.Context) ([]models.Field, error)
+	FindByUUID(context.Context, string) (*models.Field, error)
+	Create(context.Context, *models.Field) (*models.Field, error)
+	Update(context.Context, string, *models.Field) (*models.Field, error)
+	Delete(context.Context, string) error
+}
+
+func NewFieldRepository(db *gorm.DB) IFieldRepository {
+	return &FieldRepository{
+		db: db,
+	}
+}
+
+func (f *FieldRepository) FindAllWithPagination(ctx context.Context, param *dto.FieldRequestParam) ([]models.Field, int64, error) {
+	var (
+		fields []models.Field
+		sort   string
+		total  int64
+	)
+
+	if param.SortColumn != nil {
+		sort = fmt.Sprintf("%s %s", *param.SortColumn, *param.SortOrder)
+	} else {
+		sort = "created_at desc"
+	}
+
+	limit := param.Limit
+	offset := (param.Page) - 1*limit
+	err := f.db.
+		WithContext(ctx).
+		Limit(limit).
+		Offset(offset).
+		Order(sort).
+		Find(&fields).
+		Error
+	if err != nil {
+		return nil, 0, errWrap.WrapError(errConstant.ErrSQLError)
+	}
+
+	err = f.db.
+		WithContext(ctx).
+		Model(&fields).
+		Count(&total).
+		Error
+	if err != nil {
+		return nil, 0, errWrap.WrapError(errConstant.ErrSQLError)
+	}
+
+	return fields, total, nil
+}
+
+func (f FieldRepository) FindAllWithOutPagination(ctx context.Context) ([]models.Field, error) {
+	var fields []models.Field
+	err := f.db.
+		WithContext(ctx).
+		Find(&fields).
+		Error
+	if err != nil {
+		return nil, errWrap.WrapError(errConstant.ErrSQLError)
+	}
+
+	return fields, nil
+}
+
+func (f FieldRepository) FindByUUID(ctx context.Context, uuid string) (*models.Field, error) {
+	var field models.Field
+	err := f.db.
+		WithContext(ctx).
+		Where("uuid = ?", uuid).
+		First(&field).
+		Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, errWrap.WrapError(errField.ErrFieldNotFound)
+		}
+
+		return nil, errWrap.WrapError(errConstant.ErrSQLError)
+	}
+
+	return &field, nil
+}
+
+func (f FieldRepository) Create(ctx context.Context, field *models.Field) (*models.Field, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (f FieldRepository) Update(ctx context.Context, s string, field *models.Field) (*models.Field, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (f FieldRepository) Delete(ctx context.Context, s string) error {
+	//TODO implement me
+	panic("implement me")
+}
