8910833db7c5591460fd2381f2d12ab960579481
rangguy <ranggadwi100@gmail.com>
2025-08-01 23:54:53 +0700

    Repository Bagian 5

--- repositories/registry.go
+++ repositories/registry.go
@@ -1,7 +1,9 @@
 package repositories
 
 import (
-	repositories "field-service/repositories/user"
+	fieldRepo "field-service/repositories/field"
+	fieldScheduleRepo "field-service/repositories/field_schedule"
+	timeRepo "field-service/repositories/time"
 	"gorm.io/gorm"
 )
 
@@ -10,13 +12,23 @@ type Registry struct {
 }
 
 type IRepositoryRegistry interface {
-	GetUser() repositories.IUserRepository
+	GetField() fieldRepo.IFieldRepository
+	GetFieldSchedule() fieldScheduleRepo.IFieldScheduleRepository
+	GetTime() timeRepo.ITimeRepository
 }
 
 func NewRepositoryRegistry(db *gorm.DB) *Registry {
 	return &Registry{db}
 }
 
-func (r *Registry) GetUser() repositories.IUserRepository {
-	return repositories.NewUserRepository(r.db)
+func (r *Registry) GetField() fieldRepo.IFieldRepository {
+	return fieldRepo.NewFieldRepository(r.db)
+}
+
+func (r *Registry) GetFieldSchedule() fieldScheduleRepo.IFieldScheduleRepository {
+	return fieldScheduleRepo.NewFieldScheduleRepository(r.db)
+}
+
+func (r *Registry) GetTime() timeRepo.ITimeRepository {
+	return timeRepo.NewTimeRepository(r.db)
 }
